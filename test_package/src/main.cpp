8ea432fdb42237a6ad5da983a89123c880312097
Matthew Rodusek <matthew.rodusek@gmail.com>
2018-08-21 00:29:55 -0400

    Add conan test_package (#5)
    This adds a conan test_package project so that `conan create` calls 
    will not succeed if a package is not produced correctly.

---
+++ test_package/src/main.cpp
@@ -0,0 +1,13 @@
+#include "bit/math/angles.hpp"
+#include "bit/math/clamped.hpp"
+#include "bit/math/euler.hpp"
+#include "bit/math/half.hpp"
+#include "bit/math/interpolation.hpp"
+#include "bit/math/math.hpp"
+#include "bit/math/matrix.hpp"
+#include "bit/math/quaternion.hpp"
+#include "bit/math/simplex.hpp"
+#include "bit/math/transform.hpp"
+#include "bit/math/vector.hpp"
+
+int main(){}
