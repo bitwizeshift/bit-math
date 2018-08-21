b5ad0c368e4cce912b69d2fa651fddb0827a51b2
Matthew Rodusek <matthew.rodusek@gmail.com>
2018-08-20 21:53:18 -0400

    Fix ccache causing 'unused argument' errors

--- CMake/Modules/EnableCCache.cmake
+++ CMake/Modules/EnableCCache.cmake
@@ -25,7 +25,10 @@ macro(enable_ccache)
   set(__FIND_ROOT_PATH_MODE_PROGRAM)
 
   if(CCACHE_PATH)
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_PATH})
+    if(  "${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang" )
+      set(__ccache_cxx_flags -Qunused-arguments -fcolor-diagnostics)
+    endif()
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_PATH} ${__ccache_cxx_flags})
     set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ${CCACHE_PATH})
   endif()
 
